a2599ec1da3ce7ef478f7a8c0fb29b4ba49188c1
Anthony Car <agcarat@gmail.com>
2017-06-24 21:44:39 -0400

    sorted

--- main.go
+++ main.go
@@ -5,20 +5,51 @@ package main
 import (
 		"os"
 		"fmt"
+		"bufio"
+		"sort"
 		)
 
+func Readln(r *bufio.Reader) (string, error) {
+	var (isPrefix bool = true
+			err error = nil
+			line, ln []byte
+			)
+		for isPrefix && err == nil {
+			line, isPrefix, err = r.ReadLine()
+				ln = append(ln, line...)
+		}
+	return string(ln),err
+}
+
 func main() {
-	data := make([]byte, 100)
-	count, _:= os.Stdin.Read(data)
+	
+
 	var x map[byte ] int = make(map[byte]int)
-	for i := 0; i < count; i++  {
-			if _, ok := x[data[i] ]; ok {
-		  	x[data[i] ] = x[data[i]] + 1
+	r := bufio.NewReader(os.Stdin)
+	s, e := Readln(r)
+	for e == nil {
+		for i := range s {
+			if _, ok := x[s[i] ]; ok {
+				x[s[i] ] = x[s[i]] + 1
 			} else {
-		  	x[data[i] ] = 1 
+				x[s[i] ] = 1 
 			}
+		}
+		s,e = Readln(r)
 	}
-	for k, _ := range x {
-		fmt.Printf("%c=%d\n", k, x[k])
+
+	n := map[int][]byte{}
+	var a []int
+		for k, v := range x {
+			n[v] = append(n[v], k)
+		}
+	for k := range n {
+		a = append(a, k)
 	}
+	sort.Sort(sort.Reverse(sort.IntSlice(a)))
+		for _, k := range a {
+			for _, s := range n[k] {
+				fmt.Printf("%c, %d\n", s, k)
+			}
+		}
 }
